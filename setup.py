c8211b117a1cdb38f0ab0d001e25d1e38fbbd368
William Holden <wholden@users.noreply.github.com>
2017-12-24 22:25:43 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,13 +10,6 @@ setup(
     author = "William",
     description = ("Driver interface for using stepper motors with Arduino and GRBL"),
     license = "MIT",
-    # keywords = "example documentation tutorial",
-    # url = "http://packages.python.org/an_example_pypi_project",
     packages=['GrblMotors'],
-    long_description=read('README.md'),
-    # classifiers=[
-    #     "Development Status :: 3 - Alpha",
-    #     "Topic :: Utilities",
-    #     "License :: OSI Approved :: BSD License",
-    # ],
+    long_description=read('README.md')
 )
